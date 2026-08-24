140c835b8484658e93593b296ac0e48ac5660bce
左其瑞 <1204971479@qq.com>
2026-08-24 23:39:23 +0800

    chore: add src/wind_lidar/__init__.py

---
+++ src/wind_lidar/__init__.py
@@ -0,0 +1,15 @@
+"""Wind lidar analytics toolkit."""
+
+from .analytics import analyze_wind, detect_events
+from .core import LidarGeometry, reconstruct_wind
+from .io import load_lidar_csv
+
+__all__ = [
+    "LidarGeometry",
+    "analyze_wind",
+    "detect_events",
+    "load_lidar_csv",
+    "reconstruct_wind",
+]
+
+
